2be608a0a4baa0c09fe5ef7d6caae4679c592d54
Matti <matti738@hotmail.com>
2025-03-12 23:29:40 +0200

    Generate personal schedules as PDF files

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import csv
-import json
+import os
 from argparse import ArgumentParser
 from typing import TypedDict
 from datetime import date, time, datetime
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer,
+    Table,
+    TableStyle,
+    Flowable,
+)
 
 
 class EventBase(TypedDict):
@@ -31,9 +41,28 @@ type EventsByName = dict[str, Event]
 
 type SchedulesByPerson = dict[str, list[EventPersonal]]
 
+DATE_FORMAT = "%d/%m/%Y"
+
+TIME_FORMAT = "%H.%M"
+
+PAGE_MARGIN = 30
+
+styles = getSampleStyleSheet()
+spacer = Spacer(1, 12)
+table_style = TableStyle(
+    (
+        ("ALIGN", (0, 0), (-1, -1), "LEFT"),
+        ("FONTNAME", (0, 0), (-1, -1), "Helvetica"),
+        ("FONTSIZE", (0, 0), (-1, -1), 10),
+        ("LEFTPADDING", (0, 0), (-1, -1), 0),
+        ("RIGHTPADDING", (0, 0), (-1, -1), 0),
+        ("VALIGN", (0, 0), (-1, -1), "TOP"),
+    )
+)
+
 
 def get_datetime(date: str) -> datetime:
-    return datetime.strptime(date, "%d/%m/%Y %H:%M")
+    return datetime.strptime(date, f"{DATE_FORMAT} {TIME_FORMAT}")
 
 
 def add_event(
@@ -104,8 +133,13 @@ def get_schedules(
     events_by_name: EventsByName = {}
     schedules_by_person: SchedulesByPerson = {}
 
-    with open(input_file, newline="", encoding="utf-8-sig") as csvfile:
+    with open(input_file, newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
+
+        if reader.fieldnames:
+            reader.fieldnames = [
+                header.lstrip("\ufeff") for header in reader.fieldnames
+            ]
 
         for row in reader:
             event_datetime = get_datetime(row["Start time"])
@@ -152,16 +186,85 @@ def get_schedules(
     )
 
 
-def get_pretty_printed_list(list: list[Event | PersonalSchedule]) -> str:
-    return json.dumps(list, default=str, indent=4)
+def get_date(date: date) -> str:
+    return date.strftime(DATE_FORMAT)
+
+
+def get_time(time: time) -> str:
+    return time.strftime(TIME_FORMAT)
+
+
+def get_events_by_date(
+    events: list[EventPersonal],
+) -> dict[str, list[EventPersonal]]:
+    events_by_date: dict[str, list[EventPersonal]] = {}
+
+    for event in events:
+        date_str = get_date(event["date"])
+
+        if date_str not in events_by_date:
+            events_by_date[date_str] = []
+
+        events_by_date[date_str].append(event)
+
+    return events_by_date
+
+
+def build_pdf(person_name: str, events: list[EventPersonal]) -> list[Flowable]:
+    elements: list[Flowable] = []
+
+    elements.append(Paragraph(person_name, styles["Title"]))
+    elements.append(spacer)
+
+    for date_str, events in get_events_by_date(events).items():
+        elements.append(Paragraph(date_str, styles["Heading2"]))
+
+        table_data = [
+            (
+                f"{get_time(event['start_time'])} - {get_time(event['end_time'])}",
+                Paragraph(event["name"]),
+                event["group"],
+                event["place"],
+            )
+            for event in events
+        ]
+
+        if not table_data:
+            continue
+
+        table = Table(table_data, colWidths=(80, 225, 120, 100))
+
+        table.setStyle(table_style)
+        elements.append(table)
+        elements.append(spacer)
+
+    return elements
+
+
+def create_pdf(elements: list[Flowable], output_path: str):
+    SimpleDocTemplate(
+        output_path,
+        pagesize=A4,
+        leftMargin=PAGE_MARGIN,
+        rightMargin=PAGE_MARGIN,
+    ).build(elements)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser()
 
     parser.add_argument("input_filepath", type=str)
+    parser.add_argument("output_folder", type=str)
 
-    all_events, personal_schedule = get_schedules(parser.parse_args().input_filepath)
+    args = parser.parse_args()
+    all_events, personal_schedules = get_schedules(args.input_filepath)
 
-    print(get_pretty_printed_list(list(all_events)))
-    print(get_pretty_printed_list(list(personal_schedule)))
+    os.makedirs(args.output_folder, exist_ok=True)
+
+    for schedule in personal_schedules:
+        person = schedule["person"]
+
+        create_pdf(
+            build_pdf(person, schedule["events"]),
+            os.path.join(args.output_folder, f"{person}.pdf"),
+        )
